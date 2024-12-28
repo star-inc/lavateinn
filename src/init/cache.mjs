3bdf6d7cd976d1ef3805750abe5d2d0244aec81f
不知火 Shiranui <supersonic@livemail.tw>
2024-12-28 23:41:31 +0800

    fix(cache.mjs): bugs

--- src/init/cache.mjs
+++ src/init/cache.mjs
@@ -23,18 +23,23 @@ class Cache {
     /**
      * The Lavateinn cache instance.
      * @param {Redis} client - The cache client.
-     * @throws {Error} If the client is invalid.
      */
     constructor(client) {
         this._redisClient = client;
-        throw new Error("invalid client");
+    }
+
+    /**
+     * Get the raw ioredis client.
+     * @returns {any} The client.
+     */
+    rawClient() {
+        return this._redisClient;
     }
 
     /**
      * Get a cached value via its key.
      * @param {string} key - The cache key.
      * @returns {any} The cached element.
-     * @throws {Error} If the client is invalid.
      */
     get(key) {
         return this._redisClient.get(key);
@@ -44,7 +49,6 @@ class Cache {
      * Get multiple cached keys at once.
      * @param {string[]} keys - An array of cache keys.
      * @returns {any[]} An array of cached elements.
-     * @throws {Error} If the client is invalid.
      */
     mget(keys) {
         return this._redisClient.mget(keys);
@@ -56,7 +60,6 @@ class Cache {
      * @param {any} value - The value to cache.
      * @param {number} ttl - The time to live for the cache.
      * @returns {boolean} True if the key is set, false otherwise.
-     * @throws {Error} If the client is invalid.
      */
     set(key, value, ttl) {
         return this._redisClient.set(key, value, ttl);
@@ -66,7 +69,6 @@ class Cache {
      * Set multiple cached keys with the given values.
      * @param {object[]} keyValueSet - An array of object.
      * @returns {boolean} True if all keys are set, false otherwise.
-     * @throws {Error} If the client is invalid.
      */
     mset(keyValueSet) {
         return this._redisClient.mset(keyValueSet);
@@ -76,7 +78,6 @@ class Cache {
      * Delete a cached values via their keys.
      * @param {string} keys - The cache key.
      * @returns {boolean} True if the key is deleted, false otherwise.
-     * @throws {Error} If the client is invalid.
      */
     del(keys) {
         return this._redisClient.del(keys);
@@ -87,7 +88,6 @@ class Cache {
      * @param {string} key - The cache key.
      * @param {number} ttl - The time to live for the cache.
      * @returns {boolean} True if the key is set, false otherwise.
-     * @throws {Error} If the client is invalid.
      */
     ttl(key, ttl) {
         return this._redisClient.expire(key, ttl);
@@ -97,7 +97,6 @@ class Cache {
      * Get the time to live (TTL) of a cached value.
      * @param {string} key - The cache key.
      * @returns {number} The TTL in seconds.
-     * @throws {Error} If the client is invalid.
      */
     getTTL(key) {
         return this._redisClient.ttl(key);
@@ -106,7 +105,6 @@ class Cache {
     /**
      * List all keys within this cache
      * @returns {string[]} An array of all keys.
-     * @throws {Error} If the client is invalid.
      */
     keys() {
         return this._redisClient.keys("*");
@@ -115,7 +113,6 @@ class Cache {
     /**
      * Get cache statistics.
      * @returns {object[]} An array of cache statistics.
-     * @throws {Error} If the client is invalid.
      */
     getStats() {
         return this._redisClient.server_info;
@@ -124,7 +121,6 @@ class Cache {
     /**
      * Flush the whole data and reset the cache.
      * @returns {boolean} true if the cache is flushed.
-     * @throws {Error} If the client is invalid.
      */
     flushAll() {
         return this._redisClient.flushall();
@@ -133,7 +129,6 @@ class Cache {
     /**
      * This will clear the interval timeout which is set on checkperiod option.
      * @returns {boolean} true if the cache is cleared and closed.
-     * @throws {Error} If the client is invalid.
      */
     close() {
         return this._redisClient.quit();
