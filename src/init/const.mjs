eaf5e5eb3b6e067d35eefb97610e87bfa00208b1
不知火 Shiranui <supersonic@livemail.tw>
2024-12-28 23:44:35 +0800

    chore(const.mjs): update lav version

--- src/init/const.mjs
+++ src/init/const.mjs
@@ -5,7 +5,7 @@ export const APP_DESCRIPTION =
     "Tiny and flexible microservice framework for fast development.";
 
 export const APP_VERSION = "latest";
-export const LAV_VERSION = "2024.12.19";
+export const LAV_VERSION = "2024.12.28";
 
 export const APP_AUTHOR_NAME = "Star Inc.";
 export const APP_AUTHOR_URL = "https://starinc.xyz";
